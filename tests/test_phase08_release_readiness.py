11faa26d8882f39319578eca3e6df5a72539465b
hamednaghi213-del <hamednaghi213@gmail.com>
2026-09-02 19:15:37 +0330

    Include migration 021 in release readiness test
    Added '021' to the expected migration sequence in tests.

--- tests/test_phase08_release_readiness.py
+++ tests/test_phase08_release_readiness.py
@@ -32,7 +32,7 @@ def test_repository_migrations_are_contiguous():
     migrations = validate_migration_sequence(schema_dir)
     assert [item.name[:3] for item in migrations] == [
         "001", "002", "003", "004", "005", "006", "007", "008", "009",
-        "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020",
+        "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021",
     ]
 
 
